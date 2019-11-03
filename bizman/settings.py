9bc2579a6f8cedd751f5162ed9098b5121ea285b
Shivam Balwani <shivambalwani0407@gmail.com>
2019-11-03 20:10:23 +0530

    Models initialised

--- bizman/settings.py
+++ bizman/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cart.apps.CartConfig',
+    'user.apps.UserConfig',
+    'business.apps.BusinessConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
