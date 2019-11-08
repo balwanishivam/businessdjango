6d93762f205a7a298e00c181d460bc8af5ee7c13
Shivam Balwani <shivambalwani0407@gmail.com>
2019-11-08 19:55:10 +0530

    Inventory System

--- bizman/settings.py
+++ bizman/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -34,6 +33,7 @@ INSTALLED_APPS = [
     'cart.apps.CartConfig',
     'user.apps.UserConfig',
     'business.apps.BusinessConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
