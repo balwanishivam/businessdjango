ffd7831dfb9b1d772464afc4f337acd454805a5a
Shivam Balwani <shivambalwani0407@gmail.com>
2019-11-18 09:07:59 +0530

    Home page added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'accounts'
 # Be careful setting the name to just /login use userlogin instead!
 urlpatterns=[
-    path('',views.index,name="index"),
+    path('',views.home,name="home"),
+    path('index/',views.index,name="index"),
     path('register/',views.register,name="register"),
     path('user_login/',views.user_login,name='user_login'),
     #path('user_logout',views.user_login,name='logout'),
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-
+from business.views import *
+def home(request):
+    return render(request,'accounts/home.html')
 def index(request):
     return render(request,'accounts/index.html')
 @login_required
@@ -17,7 +19,7 @@ def user_logout(request):
 def register(request):
     registered = False
     if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
+        user_form = UserForm(initial={'username':  'Enter Username', 'password':'Enter Password'},data=request.POST)
         profile_form = UserProfileInfoForm(data=request.POST)
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
@@ -45,7 +47,7 @@ def user_login(request):
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
         if user:
-            if user.is_active:
+            if user.is_is_authenticated:
                 login(request,user)
                 return HttpResponseRedirect(reverse('index'))
             else:
